4834fe3e0d724954400e60ecd7020861e99e3d0b
Sanskie19 <muhammadihsanansori2@gmail.com>
2024-11-11 18:42:40 +0700

    menambahkan if,else dan print.py

--- menambahkan if,else dan print.py
+++ menambahkan if,else dan print.py
@@ -4,3 +4,6 @@
         else:
             kesempatan -= 1
             print(f"Password salah. Anda memiliki {kesempatan} kesempatan tersisa.")
+  print("Anda telah memasukkan password yang salah sebanyak 3 kali. Akses ditolak.")
+
+menu_login()   
