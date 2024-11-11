daed301375bbbc2334f3572bff7391db1b6ff5d2
Sanskie19 <muhammadihsanansori2@gmail.com>
2024-11-11 18:17:31 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def menu_login():
+    username = "Daspro2023"
+    password_benar = "Latihan"
+    
+    kesempatan = 3
+    
+    print("Selamat datang di menu login!")
+    print(f"Username: {username}")
